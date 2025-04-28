e1e63a60515e44f24e3cd6e493cea2cb090adf67
kimminseop99 <minseopkim99@gmail.com>
2025-04-29 01:36:23 +0900

    first commit

---
+++ src/main/java/com/example/BookStory/BookStoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookStory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookStory/BookStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookStory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
