bab6fd79fb2e257775fad265f57342643030d2b8
kimminseop99 <minseopkim99@gmail.com>
2025-05-03 00:41:31 +0900

    2part complete

--- src/main/java/com/example/BookStory/answer/AnswerRepository.java
+++ src/main/java/com/example/BookStory/answer/AnswerRepository.java
@@ -3,5 +3,5 @@ package com.example.BookStory.answer;
 import com.example.BookStory.question.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AnswerRepository extends JpaRepository<Question,Integer> {
+public interface AnswerRepository extends JpaRepository<Answer,Integer> {
 }
